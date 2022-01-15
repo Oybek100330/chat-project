ea101d327bb69bfafa012aa9aabd0b554b1e1da1
Oybek <allan.o.azaroff@gmail.com>
2022-01-15 17:45:38 +0500

    register module is finished

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const fs = require('fs')
+
 const { sign } = require('../utils/jwt.js')
 const sha256 = require('sha256')
 
@@ -25,6 +28,39 @@ const LOGIN = (req, res, next) => {
 
 const REGISTER = (req, res, next) => {
     try {
+        const users = req.select('users')
+        const { username, password } = req.body
+
+        const found = users.find(user => user.username == username)
+
+        if(found) throw new Error("This user already exists!")
+        if(!req.file) throw new Error("The avatar image is required")
+
+        const { size, mimetype, buffer, originalname } = req.file
+
+        if(size > 10 * (2 ** 20)) throw new Error("The avatar image is larger 10 MB")
+        if(!['image/jpg', 'image/jpeg', 'image/png'].includes(mimetype)) throw new Error("The avatar image format must be jpg, jpeg or png")
+
+        const avatarName = Date.now() + originalname.replace(/\s/g, '')
+        const pathName = path.join( process.cwd(), 'src', 'files', 'images', avatarName)
+        fs.writeFileSync(pathName, buffer)
+
+        const newUser = {
+            userId: users.length ? users[users.length - 1].userId + 1 : 1,
+            username,
+            password: sha256(password),
+            profileImg: '/images/' + avatarName
+        }
+
+        users.push(newUser)
+        req.insert('users', users)
+        delete newUser.password
+
+        res.status(201).json({
+            user: newUser,
+            message: "The user has succesfully registered",
+            token: sign({ userId: newUser.userId, agent: req.headers['user-agent'] })
+        })
 
     } catch(error) {
         next(error)
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const multer = require('multer')
 const { PORT } = require('../config.js')
 const app = express()
+const imageUpload = multer()
 
 const modelMiddleware = require('./middlewares/model.js')
+app.use(imageUpload.single('avatar'))
 app.use(express.json())
 
 const userRouter = require('./routes/user.js')
@@ -14,6 +17,7 @@ app.use('/auth', authRouter)
 
 app.use((error, req, res, next) => {
     //...
+    // console.log(error);
     res.send({message: error.message})
 })
 
